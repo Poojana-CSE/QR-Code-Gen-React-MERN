2be13eb8814f5d7102c1eb646b8f199f25ab723c
Poojana S <poojana.s2023cse@sece.ac.in>
2025-01-23 15:37:45 +0530

    CSS Completed

--- src/App.js
+++ src/App.js
@@ -23,31 +23,29 @@ const App = () => {
   }
 
   return (
-    <div>
+    <div className='qrcode'>
+    <div className='heading'>
       <h1>QR Code Gendrator</h1>
-
+    </div>
+    <div className='img'>
       {load && <p>please wait</p>} {/* conditional renderring */}
-
       <img src={image} alt='qr-box' className='QR-img'></img>
-
+    </div>
+    <div className='input'>
       <label className='input-label'>Data for QR-Code</label>
       <input type="text" id="data-input" placeholder='Enter data for Qr code' onChange={(e)=>setqrdata(e.target.value)}></input>
-
+      <br></br>
+      <br></br>
+      <br></br>
       <label className='input-label'>ImageSize(eg:150)</label>
       <input type="text" id="sizeinput" placeholder='Enter image size' onChange={(e)=>setsize(e.target.value)}></input>
-
+    </div>
+    <div className='btn'> 
       <button className="gen-btn" onClick={Qr_gen}>Gendrate QR-Code</button>
       <button className='download'         onClick={()=>{
-          const link = document.createElement("a");
-          link.href=Img;
-          link.download = "QR Code.jpg";
-          link.click();
-        }}>
-        Download QR Code
-
-      </button>
-      
+        const link = document.createElement("a"); link.href=Img; link.download = "QR Code.jpg"; link.click();}}>Download QR Code</button>
     </div>
+  </div>
   )
 }
 
